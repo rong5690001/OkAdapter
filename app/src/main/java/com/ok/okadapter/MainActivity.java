e1f205b8179b2267448519249bbb67473b174b9d
陈华榕 <map.chen@sportq.com>
2017-12-26 20:44:04 +0800

    添加基础框架

--- app/src/main/java/com/ok/okadapter/MainActivity.java
+++ app/src/main/java/com/ok/okadapter/MainActivity.java
@@ -2,12 +2,35 @@ package com.ok.okadapter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRecyclerView = findViewById(R.id.recyclerView);
+    }
+
+    public static class RecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return null;
+        }
+
+        @Override
+        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
     }
 }
---
+++ library/src/main/java/com/rong/library/OkViewHold.java
@@ -0,0 +1,41 @@
+package com.rong.library;
+
+import android.support.annotation.IdRes;
+import android.support.annotation.Nullable;
+import android.support.annotation.StringRes;
+import android.support.v7.widget.RecyclerView;
+import android.util.SparseArray;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * Created by chen.huarong on 2017/12/26.
+ */
+
+public class OkViewHold extends RecyclerView.ViewHolder {
+
+    private SparseArray<View> views = new SparseArray<>();
+
+    public OkViewHold(View itemView) {
+        super(itemView);
+    }
+
+    @Nullable
+    public <T> T findViewById(@IdRes int id) {
+        View view = views.get(id, null);
+        if (view == null) {
+            view = itemView.findViewById(id);
+            views.put(id, view);
+        }
+        return (T) view;
+    }
+
+    public void setText(@IdRes int id, @StringRes int resId) {
+        ((TextView) findViewById(id)).setText(resId);
+    }
+
+    public void setText(@IdRes int id, String res) {
+        ((TextView) findViewById(id)).setText(res);
+    }
+
+}
